3af83ffd5982dbc8f5cb1bfaaebeabef8e66d4f2
Zee <zee@zincma.de>
2019-05-19 16:41:54 -0700

    Membership validations were failing

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,20 +2,26 @@ staff_member = Person.find_or_create_by(email: 'staff@example.com')
   .tap { |o| o.update(password: 'Password123') }
 zinc_community = Community.find_or_create_by(slug: :zinc)
   .tap { |o| o.update(name: "Zinc") }
-moderator =  Person.find_or_create_by(email: 'moderator@example.com')
+
+  moderator =  Person.find_or_create_by(email: 'moderator@example.com')
   .tap { |o| o.update(password: 'Password123') }
 moderator_membership = zinc_community.memberships.find_or_create_by(person: moderator)
-  .tap { |o| o.update(status: :moderator) }
+  .tap { |o| o.update(status: :moderator, status_changed_at: Time.now,
+                      name: moderator.email, email: moderator.email) }
+
 community_member = Person.find_or_create_by(email: 'member@example.com')
   .tap { |o| o.update(password: 'Password123') }
 community_member_membership =
   zinc_community.memberships.find_or_create_by(person: community_member)
-  .tap { |o| o.update(status: :member) }
+  .tap { |o| o.update(status: :member, status_changed_at: Time.now,
+                      name: community_member.email, email: community_member.email ) }
+
 guest_member = Person.find_or_create_by(email: 'guest@example.com')
   .tap { |o| o.update(password: "Password123") }
 guest_member_membership =
   zinc_community.memberships.find_or_create_by(person: community_member)
-  .tap { |o| o.update(status: :guest) }
+  .tap { |o| o.update(status: :guest, status_changed_at: Time.now,
+                      name: guest_member.email, email: guest_member.email) }
 
 nourish_community = Community.find_or_create_by(slug: :nourish)
   .tap { |o| o.update(name: "Nourish") }
