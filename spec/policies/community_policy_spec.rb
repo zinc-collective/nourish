d0a615279042498fec2a01348f8bab4417373576
Zee <zee@zincma.de>
2019-05-19 17:03:23 -0700

    Don't prompt members to join a community they're in already

--- spec/policies/community_policy_spec.rb
+++ spec/policies/community_policy_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe CommunityPolicy do
   let(:community) { membership&.community }
   let(:person) { membership&.person }
   let(:membership) { nil }
+
   describe '#list_members?' do
     subject { policy.list_members? }
 
@@ -31,6 +32,44 @@ RSpec.describe CommunityPolicy do
     context 'when the person is a community moderator' do
       let(:membership) { create(:membership, :moderator) }
       it { is_expected.to eql true }
+    end
+  end
+
+  describe '#join?' do
+    subject { policy.join? }
+    context 'when the person is not logged in' do
+      let(:person) { nil }
+      it { is_expected.to eql true }
+    end
+
+    context 'when the person is logged in, but has no membership' do
+      let(:person) { create(:person) }
+      it { is_expected.to eql true }
+    end
+
+    context 'when the person is not approved' do
+      let(:membership) { create(:membership, :guest) }
+      it { is_expected.to eql false }
+    end
+
+    context 'when the person is an approved member' do
+      let(:membership) { create(:membership, :member) }
+      it { is_expected.to eql false }
+    end
+
+    context 'when the person is Nourish Staff' do
+      let(:person) { create(:person, :staff) }
+      it { is_expected.to eql true }
+
+      context 'and already a member of the community' do
+        let(:membership) { create(:membership, :guest, person: person) }
+        it { is_expected.to eql false }
+      end
+    end
+
+    context 'when the person is a community moderator' do
+      let(:membership) { create(:membership, :moderator) }
+      it { is_expected.to eql false }
     end
   end
 end
