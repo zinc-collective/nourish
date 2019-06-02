4504337a2217900ef5000e4ab9c072d597a113d8
Zee <zee@wecohere.com>
2019-06-02 12:07:09 -0700

    Revert "Barebone setup to send email when moderator approve memberships (#31)"
    This reverts commit 54cb6bf5e763a84ad901b28b6c82e819f57cbd67.

--- spec/mailers/previews/membership_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/membership_mailer
-class MembershipMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/membership_mailer/approve_confirmation
-  def approve_confirmation
-    MembershipMailerMailer.approve_confirmation
-  end
-
-end
